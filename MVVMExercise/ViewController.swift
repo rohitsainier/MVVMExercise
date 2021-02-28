fa67cbe3f3f1b52933d3d2b4261cc05c68208830
rohitsainier <rohitsainier@gmail.com>
2021-02-28 14:30:00 +0530

    Initial Commit

---
+++ MVVMExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExercise
+//
+//  Created by Rohit Saini on 28/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
